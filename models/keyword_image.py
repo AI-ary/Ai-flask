b5d7aac99e4218a3fccd3f7629a0fc0226a0aaf9
GaBaljaintheroom <junsu1222@naver.com>
2023-08-09 01:23:12 +0900

    refactor: keyword 필드 인덱스 생성

--- models/keyword_image.py
+++ models/keyword_image.py
@@ -6,7 +6,7 @@ from app import db
 
 class KeywordImage(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    keyword = db.Column(db.String(10), nullable=False)
+    keyword = db.Column(db.String(10), nullable=False, index=True)
     image_url = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
