925c9968a7b00b5cd9cbdc2334722fd38572cd69
GaBaljaintheroom <junsu1222@naver.com>
2023-08-09 01:23:44 +0900

    refactor: 자연어 처리 API 반환 형식 변경

--- controller/KonlpyController.py
+++ controller/KonlpyController.py
@@ -7,23 +7,17 @@ from models.keyword_image import KeywordImage
 
 @bp.route('/api/konlpy', methods=['POST'])
 def get_keyword():
-    keyword_images = []
     json_data = request.json
     contents = json_data.get('contents')
-    diary_keyword = decode(contents)  # tasks.decode.delay(contents)
+    diary_keyword = decode(contents)
 
-    while True:
-        # if not diary_keyword.ready():
-        #     time.sleep(5)
-        #     print("    delay...    ")
-        #     continue
-        # else:
-        for word in diary_keyword:
-            keyword_data_list = KeywordImage.query.filter_by(keyword=word).limit(10).all()
-            if keyword_data_list:
-                keyword_images.extend([keyword_data.image_url for keyword_data in keyword_data_list])
+    keyword_images_dict = {}
+    for word in diary_keyword:
+        keyword_data_list = KeywordImage.query.filter_by(keyword=word).limit(10).all()
+        if keyword_data_list:
+            keyword_images_dict[word] = [keyword_data.image_url for keyword_data in keyword_data_list]
 
-        return jsonify(image_urls=keyword_images)
+    return jsonify(keyword_images_dict)
 
 
 def decode(contents):
