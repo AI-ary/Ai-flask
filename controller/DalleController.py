5d4a9d96c010774d1abe5509b6fefe19d4c8f67b
GaBaljaintheroom <junsu1222@naver.com>
2023-08-10 14:17:19 +0900

    feat: Dalle API celery 연결

--- controller/DalleController.py
+++ controller/DalleController.py
@@ -1,37 +1,29 @@
 from flask import request, jsonify
+import time
 import openai
 import os
-from googletrans import Translator
-
 from controller import bp
+from tasks import make_image
 
 # OpenAI API Key 설정
 openai.api_key = os.getenv('OPEN_AI_KEY')
 
-DRAW_CUTE_CHARACTER = "Draw it as a clean, simple and cute character, ultra-detailed, 8k, " \
-                      "realistic drawing, digital art"
-
 
 @bp.route('/api/dalle', methods=['POST'])
 def generate_image():
-    # POST 요청으로 받은 JSON 데이터에서 'story' 키의 값을 가져옵니다.
+    global image_urls
     json_data = request.json
     story = json_data.get("story")
+    image_data = make_image.delay(story, os.getenv('OPEN_AI_KEY'))
 
-    # 번역기를 만든다.
-    translator = Translator()
-    # 영어로 번역
-    story_en = translator.translate(story + DRAW_CUTE_CHARACTER, 'en').text
-
-    # OpenAI API를 사용하여 이미지 생성 요청
-    response = openai.Image.create(
-        prompt=story_en,
-        n=4,
-        size="1024x1024"
-    )
-
-    # 생성된 이미지의 URL을 추출합니다.
-    image_urls = [data['url'] for data in response['data']]
-
+    while True:
+        if not image_data.ready():
+            time.sleep(5)
+            print("    delay...    ")
+            continue
+        else:
+            image_results = image_data.get()  # 결과를 가져옴
+            image_urls = [data['url'] for data in image_results['data']]
+            break
     # 이미지 URL을 JSON 형태로 응답합니다.
-    return jsonify({'image_url': image_urls})
+    return jsonify({'image_urls': image_urls})
