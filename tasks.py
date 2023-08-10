28403fa99ad7aaf6dfbe57096c6565513ce419b8
GaBaljaintheroom <junsu1222@naver.com>
2023-08-10 12:04:39 +0900

    feat: Kkma() celery 비동기 처리 tasks 작성

--- tasks.py
+++ tasks.py
@@ -1,12 +1,17 @@
-# from celery import Celery
-# from konlpy.tag import Kkma
-#
-# # app = Celery('config',  backend='rpc://', broker='amqp://gdiary:gdiary123@gdiary_host/gdiary_host', include=['text.views'])
+from celery import Celery
+from konlpy.tag import Kkma
+
+# app = Celery('config',  backend='rpc://', broker='amqp://gdiary:gdiary123@gdiary_host/gdiary_host', include=['text.views'])
 # celery = Celery('tasks', backend='rpc://', broker='pyamqp://guest@gd_rabbitmq//', include=["tasks"])
-#
-#
-# @celery.task
-# def decode(contents):
-#     analyzer = Kkma()
-#     nouns = analyzer.nouns(contents)
-#     return nouns
+
+celery = Celery('tasks',
+                broker='pyamqp://guest:guest@rabbit:5672/',
+                backend='rpc://',
+                include=["tasks"])
+
+
+@celery.task
+def decode(contents):
+    analyzer = Kkma()
+    nouns = analyzer.nouns(contents)
+    return nouns
