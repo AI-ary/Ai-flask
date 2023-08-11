4debd8e68c63a715542b4492e6e4d551c9ec7eaa
GaBaljaintheroom <junsu1222@naver.com>
2023-08-11 13:27:07 +0900

    feat: rabbitmq 설정 축가

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from config import databaseConfig
+from config.databaseConfig import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_SCHEMA
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -22,5 +23,13 @@ def create_app():
     from controller import DalleController, KonlpyController
     app.register_blueprint(DalleController.bp, name='dalle')
     app.register_blueprint(KonlpyController.bp, name='konlpy')
+
+    # rabbitmq
+    app.config.update(
+        broker='pyamqp://guest:guest@rabbit:5672/',
+        backend='db+mysql://{}:{}@{}:{}/{}?charset=utf8'
+                .format(DB_USERNAME, DB_PASSWORD,
+                        DB_HOST, DB_PORT, DB_SCHEMA)
+    )
 
     return app
