ea4b3a1dc2de6c1c7288bce089fd3b3301c56b60
GaBaljaintheroom <junsu1222@naver.com>
2023-09-12 15:14:09 +0900

    refactor: model 마이그레이션

--- controller/KonlpyController.py
+++ controller/KonlpyController.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 
 from controller import Konlpy
-from models.keyword_image import KeywordImage
+from models.keyword_image import Drawings
 from tasks import celery
 from flask_restx import Resource, fields
 
@@ -49,9 +49,9 @@ class GetKonlpyResult(Resource):
         task_id = request.json.get("task_id")
         diary_keyword = celery.AsyncResult(task_id).result
 
-        keyword_images_dict = {}
+        drawings_images_dict = {}
         for word in diary_keyword:
-            keyword_data_list = KeywordImage.query.filter_by(keyword=word).limit(10).all()
-            if keyword_data_list:
-                keyword_images_dict[word] = [keyword_data.image_url for keyword_data in keyword_data_list]
-        return jsonify(keyword_images_dict)
+            drawings_data_list = Drawings.query.filter_by(keyword=word).limit(10).all()
+            if drawings_data_list:
+                drawings_images_dict[word] = [keyword_data.image_url for keyword_data in drawings_data_list]
+        return jsonify(drawings_images_dict)
--- models/keyword_image.py
+++ models/keyword_image.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from app import db
 
 
-class KeywordImage(db.Model):
+class Drawings(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     keyword = db.Column(db.String(10), nullable=False, index=True)
     image_url = db.Column(db.String(500), nullable=False)
